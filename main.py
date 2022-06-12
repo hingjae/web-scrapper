f8ccfae10f970c405bd55a08299b309342643698
leejae honey <ljh23270@naver.com>
2022-06-12 12:49:50 +0900

    I need to  line 19

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+from notice import extract_notice_page, extract_notice_contents
 
-notice = requests.get("https://it.jbnu.ac.kr/it/9842/subview.do")
+last_notice_page = extract_notice_page()
 
-soup = BeautifulSoup(notice.text, "html.parser")
-# print(soup)
-
-pagination = soup.find("div", {"class":"_paging"})
-# print(pagination)
-
-links = pagination.find_all('li')
-# print(links)
-
-pages = []
-
-#page number 추출
-for link in links:
-    pages.append(int(link.string)) # 안에 있는 string만 가져옴 
-#span의 경우 pages.append(link.("span").string)
-print(pages[1:])
-
-max_page = pages[-1]
+extract_notice_contents(last_notice_page)
