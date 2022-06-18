fdf9596b3e75993a0a80b59ce0daf197e63d1783
leejae honey <ljh23270@naver.com>
2022-06-18 16:59:58 +0900

    extract_job

--- indeed.py
+++ indeed.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 LIMIT = 50
-URL = "https://www.indeed.com/jobs?as_and=python&limit={LIMIT}&vjk=e80e9fb4813ea766"
+URL = "https://www.indeed.com/jobs?as_and=python&limit={LIMIT}"
 def extract_indeed_pages():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
@@ -18,7 +18,18 @@ def extract_indeed_pages():
     max_page = pages[-1]
     return max_page
 
+def extract_job(html):
+    title = html.find("h2", {"class":"jobTitle"}).find("a").find("span")["title"]
+    company = html.find("span", {"class":"companyName"}).string
+    return {'title': title, 'company': company}
+
 def extract_indeed_jobs(last_page):
-    for page in range(last_page):
-        result = requests.get(f"{URL}&start={page*LIMIT}")
-        print(result.status_code) #status_code == 동작 확인
+    jobs = []
+    # for page in range(last_page):
+    result = requests.get(f"{URL}&start={0*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class" :"cardOutline"})
+    for result in results:
+        job = extract_job(result)
+        print(job)
+    return jobs
