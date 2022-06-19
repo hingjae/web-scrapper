a20c52e611b47bbfab8348994fe4360b77e937cb
leejae honey <ljh23270@naver.com>
2022-06-19 23:27:18 +0900

    indeed done so start

--- indeed.py
+++ indeed.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 LIMIT = 50
 URL = "https://www.indeed.com/jobs?as_and=python&limit={LIMIT}"
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
     pagination = soup.find("div", {"class":"pagination"})
@@ -21,15 +21,23 @@ def extract_indeed_pages():
 def extract_job(html):
     title = html.find("h2", {"class":"jobTitle"}).find("a").find("span")["title"]
     company = html.find("span", {"class":"companyName"}).string
-    return {'title': title, 'company': company}
+    location = html.find("div", {"class":"companyLocation"}).text
+    job_id = html.find("h2",{"class":"jobTitle"}).find("a")["data-jk"]
+    return {'title': title, 'company': company, 'location': location, 'link':f"https://www.indeed.com/viewjob?jk={job_id}"}
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
     jobs = []
-    # for page in range(last_page):
-    result = requests.get(f"{URL}&start={0*LIMIT}")
-    soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all("div", {"class" :"cardOutline"})
-    for result in results:
-        job = extract_job(result)
-        print(job)
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class" :"cardOutline"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
+
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
     return jobs
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from indeed import extract_indeed_pages, extract_indeed_jobs
-last_indeed_page = extract_indeed_pages()
-extract_indeed_jobs(last_indeed_page)
+from indeed import get_jobs as get_indeed_jobs
+
+indeed_jobs = get_indeed_jobs()
+
+print(indeed_jobs)
