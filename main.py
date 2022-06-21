c2702a3a2cb8d771ed41d0f518f8a2fa0a188e36
leejae honey <ljh23270@naver.com>
2022-06-21 15:32:00 +0900

    add csv file

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from indeed import get_jobs as get_indeed_jobs
 from so import get_jobs as get_so_jobs
 from save import save_to_file
 
-# indeed_jobs = get_indeed_jobs()
 so_jobs = get_so_jobs()
-print(so_jobs)
-# save_to_file(jobs)
+indeed_jobs = get_indeed_jobs()
+jobs = indeed_jobs + so_jobs
+# print(indeed_jobs)
+# print(so_jobs)
+save_to_file(jobs)
