72278bc9c1e7a0edd44fd0615594ed142debacae
leejae honey <ljh23270@naver.com>
2022-06-25 20:31:33 +0900

    setting webpage

--- main.py
+++ main.py
@@ -16,16 +16,34 @@ def report():
     word = request.args.get('word')
     if word:
         word = word.lower()
-        fromDb = db.get(word)
-        if fromDb:
-            jobs = fromDb
+        existingJobs = db.get(word)
+        if existingJobs:
+            jobs = existingJobs
         else:
             jobs = get_indeed_jobs(word)        
             db[word] = jobs
     else:
         return redirect("/")
-    return render_template("report.html", searchingBy=word, resultsNumber=len(jobs))
+    return render_template(
+        "report.html", 
+        searchingBy=word, 
+        resultsNumber=len(jobs),
+        jobs = jobs
+    )
 
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        return f"Generate CSV for {word}"
+    except:
+        return redirect("/")
 
 app.run(host="0.0.0.0")
 
